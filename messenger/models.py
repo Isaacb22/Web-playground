5cb540637a235b040196f8b13cd6f6baca6cd2a3
Isaacb22 <ibonalde001@gmail.comgit config --global user.name Isaacb22git config --global user.email ibonalde001@gmail.com>
2019-01-09 14:59:08 -0400

    push final

--- messenger/models.py
+++ messenger/models.py
@@ -34,8 +34,13 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
 	users = models.ManyToManyField(User, related_name='threads')
 	messages = models.ManyToManyField(Message)
+	updated = models.DateTimeField(auto_now=True)
+	
 
 	objects = ThreadManager()
+
+	class Meta:
+		ordering = ['-updated']
 
 def messages_changed(sender, **kwargs):
 	instance = kwargs.pop("instance", None)
@@ -54,6 +59,9 @@ def messages_changed(sender, **kwargs):
 
 	# Buscar los mensajes de false_pk_set que si estan en pk_set y los borramos de pk_set
 	pk_set.difference_update(false_pk_set) 
+
+	# Forzar la actualizacion haciendo save
+	instance.save()
 
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
--- messenger/views.py
+++ messenger/views.py
@@ -4,7 +4,9 @@ from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView	
 from django.http import Http404, JsonResponse
 from .models import Thread, Message
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 
 from django.contrib.auth.decorators import login_required
@@ -35,7 +37,14 @@ def add_message(request, pk):
 			message = Message.objects.create(user=request.user, content=content)
 			thread.messages.add(message)
 			json_response['created'] = True
-
+			if len(thread.messages.all()) is 1:
+				json_response['first'] = True
 	else:
 		raise Http404("User is not authenticated")
 	return JsonResponse(json_response)
+
+@login_required
+def start_thread(request, username):
+	user = get_object_or_404(User, username=username)
+	thread = Thread.objects.find_or_create(user, request.user)
+	return redirect(reverse_lazy('messenger:detail', args=[thread.pk]))
